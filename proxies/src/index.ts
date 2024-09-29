4e80c17373a47ebda4b5b4631bc60dfeae4cb4f4
sendo-kakeru <sendokakeru.js@gmail.com>
2024-09-29 15:28:42 +0900

    Revert "feat: 限定公開設定"
    This reverts commit ea324c28ef9ec4729ec9a613a9f71ff57a743651.

--- proxies/src/index.ts
+++ proxies/src/index.ts
@@ -1,13 +1,8 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { Env } from "hono/types";
+import {  Env } from "hono/types";
 
-const app = new Hono<{
-  Bindings: Env & {
-    ORIGIN_URL: string;
-    GOOGLE_CLOUD_PRINT_IDENTITY_TOKEN: string;
-  };
-}>();
+const app = new Hono<{ Bindings: Env & { ORIGIN_URL: string } }>();
 
 app.use(
   "*",
@@ -23,16 +18,8 @@ app.use(
 app.all("*", async (c) => {
   const url = new URL(c.req.url);
   const ORIGIN_URL = c.env.ORIGIN_URL;
-  const GOOGLE_CLOUD_PRINT_IDENTITY_TOKEN =
-    c.env.GOOGLE_CLOUD_PRINT_IDENTITY_TOKEN;
   const newUrl = ORIGIN_URL + url.pathname + url.search;
-  // 限定公開
-  const res = await fetch(newUrl, {...c.req.raw, headers: {
-    ...c.req.raw.headers,
-    Authorization: `Bearer ${GOOGLE_CLOUD_PRINT_IDENTITY_TOKEN}`,
-  }});
-  // 公開時
-  // const res = await fetch(newUrl, c.req.raw);
+  const res = await fetch(newUrl, c.req.raw);
   return new Response(res.body, {
     ...res,
     webSocket: res.webSocket ?? undefined,
